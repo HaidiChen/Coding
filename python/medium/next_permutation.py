5cf8936f12a2804212bbef921f68ce084bfdcfe5
Kakaluoto <chenhaidi43@163.com>
2019-03-28 21:28:08 -0500

    edge cases considered

--- python/medium/next_permutation.py
+++ python/medium/next_permutation.py
@@ -1,8 +1,15 @@
 def np(arr):
     n = len(arr)
+    if n < 2:
+        print(arr)
+        return
     i = n - 2
-    while arr[i] >= arr[i + 1]:
+    while arr[i] >= arr[i + 1] and i >= 0:
         i = i - 1
-    arr[i], arr[i + 1] = arr[i + 1], arr[i]
-    arr[i + 1 : n] = sorted(arr[i + 1 : n])
-    print(arr)
+    if i < 0:
+        print(sorted(arr))
+        return
+    else:
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        arr[i + 1 : n] = sorted(arr[i + 1 : n])
+        print(arr)
